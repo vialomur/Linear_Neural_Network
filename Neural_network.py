f31c40500ece000c439d2f7f4b2853e7cefd0280
vialomur <maxim44m@ukr.net>
2023-08-30 14:20:19 +0200

    Implemented stop function based on error percentage

--- Neural_network.py
+++ Neural_network.py
@@ -66,24 +66,39 @@ class NeuralNetwork2:
         self.weights = weights
         self.bias = bias
         self.learning_rate = learning_rate
+        self.stop = False
 
     def predict(self, input_data):
         return sum(input_data[0] * self.weights) + self.bias
 
-    def train(self, input_data, desired_output, epochs, test_input, test_prediction, verbose=0):
+    def stop_test(self, correct_output, prediction, error_percentage):
+        if (((abs(correct_output - prediction)) / correct_output) * 100) < error_percentage:
+            self.stop = True
+
+    def train(self, input_data, desired_output, epochs, test_input, test_prediction, verbose=0, error_percentage=5):
         epoch_list = []
         error_list = []
         bias_list = []
         weight_list = []
 
         for epoch in range(epochs):
+            if self.stop:
+                break
+
             epoch_list.append(epoch)
 
-            if verbose:
+            if verbose == 1:
                 print(f"Epoch: {epoch}, prediction: {self.predict(test_input)}")
+
+            if verbose == 2:
+                if epoch % 10 == 0:
+                    print(f"Epoch: {epoch}, prediction: {self.predict(test_input)}")
 
             prediction = 0
             for i in range(len(input_data)):
+                if self.stop:
+                    break
+
                 prediction = self.predict(input_data[i])
 
                 error = desired_output[i] - prediction
@@ -93,10 +108,11 @@ class NeuralNetwork2:
 
                 self.bias += self.learning_rate * error
 
-            total_error = abs(test_prediction - prediction)
-            error_list.append(total_error)
+            #total_error = abs(test_prediction - prediction) TODO fix error appending
+            #error_list.append(total_error)
             bias_list.append(self.bias)
             weight_list.append(self.weights[:])  # Store a copy of the weights
+            self.stop_test(test_prediction, self.predict(test_input), error_percentage)
 
         final_prediction = self.predict(test_input)
         print("Final Prediction:", final_prediction)
--- n_inputs_1_output.py
+++ n_inputs_1_output.py
@@ -4,13 +4,13 @@ from Neural_network import NeuralNetwork2, correct_output
 
 # Define the model parameters
 number_of_neurons = 3
-number_of_input_data = 4
+number_of_input_data = 8
 weights = np.array([randint(0, 10) / 10 for _ in range(number_of_neurons)])
 print(weights)
 
 bias = 0.2
-learning_rate = 0.01
-epochs = 25
+learning_rate = 0.001
+epochs = 500
 
 input_data = np.array([[randint(1, 100) / 100 for _ in range(number_of_neurons)] for __ in range(number_of_input_data)])
 print(f"Input data: {input_data}")
@@ -30,4 +30,4 @@ print(f"test_prediction data: {test_prediction}")
 
 # Create and train the neural network
 nn = NeuralNetwork2(weights, bias, learning_rate)
-nn.train(input_data, desired_output, epochs, test_input, test_prediction)
+nn.train(input_data, desired_output, epochs, test_input, test_prediction, verbose=2)
