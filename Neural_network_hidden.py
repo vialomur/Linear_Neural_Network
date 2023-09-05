4b1f84734a287c1eabe42c6d4497ceccb9a5e977
vialomur <maxim44m@ukr.net>
2023-09-05 22:02:56 +0200

    Trying to refactor code to make it more clean and better

--- Neural_network_hidden.py
+++ Neural_network_hidden.py
@@ -1,37 +1,25 @@
-import multiprocessing
-import random
-
+from random import randint
 import numpy as np
 
 
-class NeuralNetwork3:
-    def __init__(self, weights, hidden_weights, bias, learning_rate):
-        self.weights = weights
-        self.hidden_weights = hidden_weights
-        self.bias = bias
-        self.bias_hidden = random.randint(0, 100) / 100
+class InputLayer:
+    def __init__(self, input_data, learning_rate):
+        self.values = input_data
+        self.weights = np.array([randint(0, 100) / 100 for _ in range(len(input_data))])
+        self.bias = randint(0, 100) / 100
         self.learning_rate = learning_rate
-        self.stop = False
         # Adam optimizer parameters
         self.beta1 = 0.9
         self.beta2 = 0.999
         self.epsilon = 1e-8
-        self.m_weights = np.zeros_like(weights)
-        self.v_weights = np.zeros_like(weights)
+        self.m_weights = np.zeros_like(self.weights)
+        self.v_weights = np.zeros_like(self.weights)
         self.m_bias = 0
         self.v_bias = 0
         self.t = 0  # Time step
 
-    def sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def predict(self, input_data, weights, bias):
-        return np.dot(input_data, weights) + bias
-
-    def stop_test(self, correct_output, prediction, error_percentage):
-        epsilon = 1e-9
-        if np.all((((abs(correct_output - prediction)) / (correct_output + epsilon)) * 100) < error_percentage):
-            self.stop = True
+    def predict(self):
+        return np.dot(self.values, self.weights) + self.bias
 
     def update_one_weight(self, input_data, error, j):
         # Compute gradients
@@ -48,38 +36,40 @@ class NeuralNetwork3:
         # Update weights
         self.weights[j] += self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
 
-    def train_one_input_data(self, input_data, desired_output):
-        hidden_output = np.dot(input_data, self.hidden_weights) + self.bias_hidden
-        hidden_output = self.sigmoid(hidden_output)  # Apply activation function
-
-        prediction = np.dot(hidden_output, self.weights) + self.bias
-        error = desired_output - prediction
-
-        # Backpropagation and weight updates for the output layer
-        delta_output = -error
-        d_weights = np.outer(hidden_output, delta_output)
-        d_bias = delta_output
-
-        # Backpropagation and weight updates for the hidden layer
-        delta_hidden = np.dot(delta_output, self.weights.T) * hidden_output * (1 - hidden_output)
-        d_hidden_weights = np.outer(input_data, delta_hidden)
-        d_bias_hidden = delta_hidden
-
-        # Update weights and biases
-        self.weights -= self.learning_rate * d_weights
-        self.bias -= self.learning_rate * d_bias
-        self.hidden_weights -= self.learning_rate * d_hidden_weights
-        self.bias_hidden -= self.learning_rate * d_bias_hidden
-        prediction = self.predict(input_data, self.hidden_weights,self.bias_hidden)  # output prediction
-
+    def train_one_input_data(self, desired_output):
+        prediction = self.predict()
         error = desired_output - prediction
 
         self.t += 1  # Increment the time step
 
         j = 0
         while j < len(self.weights):
-            self.update_one_weight(input_data, error, j)
+            self.update_one_weight(self.values, error, j)
             j += 1
+
+
+class HiddenLayer:
+    def __init__(self, prediction_from_prev_layer, size):
+        self.values = np.array([prediction_from_prev_layer for _ in range(size)])
+        self.weights = np.array([randint(0, 100) / 100 for _ in range(size)])
+        self.bias = randint(0, 100) / 100
+
+    def predict(self):
+        return np.dot(self.values, self.weights) + self.bias
+
+
+class NeuralNetwork3:
+    def __init__(self, input_data: np.array, learning_rate: float):
+        self.input_layer = InputLayer(input_data, learning_rate)
+        self.hidden_layer = HiddenLayer(self.input_layer.predict(),  len(self.input_layer.weights))
+        self.learning_rate = learning_rate
+        self.stop = False
+
+    def stop_test(self, correct_output, prediction, error_percentage):
+        epsilon = 1e-9
+        if np.all((((abs(correct_output - prediction)) / (correct_output + epsilon)) * 100) < error_percentage):
+            self.stop = True
+
 
     def train(self, input_data, desired_output, epochs, test_input, test_prediction, verbose=0, error_percentage=5):
         for epoch in range(epochs):
